49722b8476f3a23dfff565a74e87382387b255fb
serious_tom <cerniauskas.tomas68@gmail.com>
2022-11-06 02:31:39 +0200

    migrations

---
+++ database/migrations/2022_11_06_002253_create_dividends_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dividends', function (Blueprint $table) {
+            $table->id();
+            $table->double('amount', null, 2)->unsigned();
+            $table->double('taxes_amount', null, 2)->unsigned()->default(0);
+            $table->integer('user_id')->unsigned();
+            $table->integer('company_id')->unsigned();
+            $table->date('date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('dividends');
+    }
+};
